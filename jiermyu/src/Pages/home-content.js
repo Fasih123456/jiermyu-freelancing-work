d8a1f6b95b869c9dfae987a345406aefc80e0cd1
Fasih <fasihislam1999@gmail.com>
2022-07-18 21:46:56 +0400

    finished first section

--- jiermyu/src/Pages/home-content.js
+++ jiermyu/src/Pages/home-content.js
@@ -20,12 +20,20 @@ const Home = () => {
                 BAYC is a collection of 10,000 Bored Ape NFTs—unique digital collectibles living on the Ethereum blockchain. Your Bored Ape doubles as your Yacht Club membership card, and grants access to members-only benefits, the first of which is access to THE BATHROOM, a collaborative graffiti board. Future areas and perks can be unlocked by the community through roadmap activation.
                 </p>
             </Col>
-            <Col sm={4}>2 of 2</Col>
+            <Col sm={4}>        
+                <Row>
+                    <Col><img class="img-fluid" src="https://ik.imagekit.io/bayc/assets/ape1.png" alt="ape1" aria-label="ape1" /></Col>
+                    <Col><img class="img-fluid" src="https://ik.imagekit.io/bayc/assets/ape2.png" alt="ape2" aria-label="ape2" /></Col>
+                </Row>
+                <Row>
+                    <Col><img class="img-fluid" src="https://ik.imagekit.io/bayc/assets/ape3.png" alt="ape3" aria-label="ape3"/ ></Col>
+                    <Col><img class="img-fluid" src="https://ik.imagekit.io/bayc/assets/ape4.png" aria-label="ape4" /></Col>
+                </Row>
+            </Col>
         </Row>
         <Row>
             <Col>1 of 3</Col>
             <Col>2 of 3</Col>
-            <Col>3 of 3</Col>
         </Row>
         </Container>
 
