5493ae766b50b71c85d6e6955483fdcd693908c2
Fasih <fasihislam1999@gmail.com>
2022-07-18 19:57:22 +0400

    added bootstrap

--- jiermyu/src/Pages/App.js
+++ jiermyu/src/Pages/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import Home from './Home';
+import home from './home-content';
 
 import '../css/App.css';
 
@@ -9,7 +9,7 @@ const App = () => {
       <div>
           <BrowserRouter>
             <React.Fragment>
-              <Route path="/" exact  component={Home} />
+              <Route path="/" exact  component={home} />
             </React.Fragment>
           </BrowserRouter>
       </div>
