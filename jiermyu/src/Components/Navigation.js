601d9c0696316c9dbcc74c0527903b1b681e0f53
Fasih <fasihislam1999@gmail.com>
2022-07-26 12:12:22 +0400

    fixed another bug

--- jiermyu/src/Components/Navigation.js
+++ jiermyu/src/Components/Navigation.js
@@ -21,26 +21,33 @@ class Navigation extends React.Component{
                         <li class="nav-item"><a href="#buy-an-ape" class="nav-link px-lg-3 py-3 py-lg-4" ><p class="navigation-link-text">Buy An Ape</p></a></li>
                         <li class="nav-item"><a href="#roadmap"  class="nav-link px-lg-3 py-3 py-lg-4"><p class="navigation-link-text">Road Map</p></a></li>
                         <li class="nav-item"><a href="#team" class="nav-link px-lg-3 py-3 py-lg-4"><p class="navigation-link-text">Team</p></a></li>
-                        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4"><Link  style={{color: "white "}} to="/FAQ" id='page4'><p class="navigation-link-text fontaswesome"><FontAwesomeIcon icon="fa-brands fa-youtube" /></p></Link></a></li>
+                        <li class="nav-item icon"><a class="nav-link px-lg-3 py-3 py-lg-4"><Link  style={{color: "white "}} to="/FAQ" id='page4'><p class="navigation-link-text fontaswesome"><FontAwesomeIcon icon="fa-brands fa-youtube" /></p></Link></a></li>
 
-                        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4"><Link  style={{color: "white "}} to="/FAQ" id='page4'><p class="navigation-link-text fontaswesome"><FontAwesomeIcon icon="fa-brands fa-instagram" /></p></Link></a></li>
+                        <li class="nav-item icon"><a class="nav-link px-lg-3 py-3 py-lg-4"><Link  style={{color: "white "}} to="/FAQ" id='page4'><p class="navigation-link-text fontaswesome"><FontAwesomeIcon icon="fa-brands fa-instagram" /></p></Link></a></li>
 
-                        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4"><Link  style={{color: "white "}} to="/FAQ" id='page4'><p class="navigation-link-text fontaswesome "><FontAwesomeIcon icon="fa-brands fa-discord" /></p></Link></a></li>
+                        <li class="nav-item icon"><a class="nav-link px-lg-3 py-3 py-lg-4"><Link  style={{color: "white "}} to="/FAQ" id='page4'><p class="navigation-link-text fontaswesome "><FontAwesomeIcon icon="fa-brands fa-discord" /></p></Link></a></li>
 
-                        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4"><Link  style={{color: "white "}} to="/FAQ" id='page4'><p class="navigation-link-text fontaswesome"><FontAwesomeIcon icon="fa-brands fa-twitter" /></p></Link></a></li>
+                        <li class="nav-item icon"><a class="nav-link px-lg-3 py-3 py-lg-4"><Link  style={{color: "white "}} to="/FAQ" id='page4'><p class="navigation-link-text fontaswesome"><FontAwesomeIcon icon="fa-brands fa-twitter" /></p></Link></a></li>
                     </ul>
                 </React.Fragment>
             );
         }else {
             this.state.renderComponent = (
                 <React.Fragment>
-                    
                     <ul  class="navbar-nav ms-auto py-4 py-lg-0">
-                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4 " ><Link style={{color: "white "}} to="/" id='page1'><p class="navigation-link-text">Home</p></Link></a></li>
+
+                        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4 " ><Link style={{color: "white "}} to="/" id='page1'><p class="navigation-link-text">Home</p></Link></a></li>
                         <li class="nav-item"><a href="#buy-an-ape" class="nav-link px-lg-3 py-3 py-lg-4" ><p class="navigation-link-text">Buy An Ape</p></a></li>
                         <li class="nav-item"><a href="#roadmap"  class="nav-link px-lg-3 py-3 py-lg-4"><p class="navigation-link-text">Road Map</p></a></li>
                         <li class="nav-item"><a href="#team" class="nav-link px-lg-3 py-3 py-lg-4"><p class="navigation-link-text">Team</p></a></li>
-</ul>
+                        <li class="nav-item icon"><a class="nav-link px-lg-3 py-3 py-lg-4"><Link  style={{color: "white "}} to="/FAQ" id='page4'><p class="navigation-link-text fontaswesome"><FontAwesomeIcon icon="fa-brands fa-youtube" /></p></Link></a></li>
+
+                        <li class="nav-item icon"><a class="nav-link px-lg-3 py-3 py-lg-4"><Link  style={{color: "white "}} to="/FAQ" id='page4'><p class="navigation-link-text fontaswesome"><FontAwesomeIcon icon="fa-brands fa-instagram" /></p></Link></a></li>
+
+                        <li class="nav-item icon"><a class="nav-link px-lg-3 py-3 py-lg-4"><Link  style={{color: "white "}} to="/FAQ" id='page4'><p class="navigation-link-text fontaswesome "><FontAwesomeIcon icon="fa-brands fa-discord" /></p></Link></a></li>
+
+                        <li class="nav-item icon"><a class="nav-link px-lg-3 py-3 py-lg-4"><Link  style={{color: "white "}} to="/FAQ" id='page4'><p class="navigation-link-text fontaswesome"><FontAwesomeIcon icon="fa-brands fa-twitter" /></p></Link></a></li>
+                    </ul>
                 </React.Fragment>
             );
         }
